2b2d99856682946bc5fd42688b0af7506061003c
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-09-29 14:41:32 -0400

    Write insert method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -21,6 +21,46 @@ class Tree
     root
   end
 
+  def insert(data, node = @root)
+    comparison = node.data <=> data
+    case comparison
+    when -1
+      return node.right_child = create_node(data) if node.right_child.nil?
+
+      insert(data, node.right_child)
+    when 1
+      return node.left_child = create_node(data) if node.left_child.nil?
+
+      insert(data, node.left_child)
+    end
+  end
+
+#  def insert(node = @root, data)
+#    if data > node.data
+#      if node.right_child.nil?
+#        node.right_child = create_node(data)
+#      else
+#        insert(node.right_child, data)
+#      end
+#    elsif data < node.data
+#      if node.left_child.nil?
+#        node.left_child = create_node(data)
+#      else
+#        insert(node.left_child, data)
+#      end
+#    end
+#  end
+
+  def create_node(data)
+    Node.new(data)
+  end
+
+  def last_node?(node)
+    return true if node.left_child.nil? && node.right_child.nil?
+
+    false
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,13 @@
 require_relative 'binary_search_tree'
 
-test_array = (Array.new(15) { rand(1..100) })
+test_array = [4, 8, 15, 16, 23, 42, 69, 93, 7, 64, 71, 99, 13, 66, 55]
 
 p test_array.uniq.sort
 
 binary_search_tree = Tree.new(test_array)
+
+binary_search_tree.pretty_print
+
+binary_search_tree.insert(3)
 
 binary_search_tree.pretty_print
